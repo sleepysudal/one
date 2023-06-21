9c00b4bd5e390796ddecb5ec9614d949ec1987c6
unknown <kyjyoung3997@naver.com>
2023-06-21 21:55:34 +0900

    0955

---
+++ javawork/SistJavaProject/src/gittest/Test_01.java
@@ -0,0 +1,20 @@
+package gittest;
+
+import java.util.Scanner;
+
+public class Test_01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		
+		System.out.println("입력");
+		n=sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			
+		}
+		System.out.println();
+	}
+
+}
